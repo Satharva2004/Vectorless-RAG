3fa54fdd96c8dd84342805e870f88d5970926627
Satharva2004 <satharva2004@gmail.com>
2026-03-21 22:24:24 +0530

    ready for destroyment (deployment)

---
+++ backend/api/index.py
@@ -0,0 +1,5 @@
+from app.main import app
+
+# This is the entry point for Vercel
+# Vercel's Python runtime treats anything in api/*.py as a Lambda function.
+# Here's where we import the app and export it.
